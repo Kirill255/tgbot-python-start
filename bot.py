e9aa64b7f4188df484f360f12d649ab18a9facfb
Kirill255 <jhgbnm1@mail.ru>
2018-11-09 14:17:50 +0300

    prepare to deploy

--- bot.py
+++ bot.py
@@ -1,27 +1,27 @@
 import os
 import random
-import time
-import logging
+# import time
+# import logging
 import telebot
 from telebot import types
-from telebot import apihelper
+# from telebot import apihelper
 
 # Logger
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG)
+# logger = telebot.logger
+# telebot.logger.setLevel(logging.DEBUG)
 
 # http -> proxy, что-то в данный момент с простым proxy у этого модуля проблемы
 # PROXY = "http://173.192.21.89:8123"
 # apihelper.proxy = {"http": PROXY}
 
 # https -> socks proxy, с socks всё работает
-PROXY = "socks5://85.105.213.27:9999"
+# PROXY = "socks5://85.105.213.27:9999"
 # PROXY = os.environ.get("PROXY")
-apihelper.proxy = {"https": PROXY}
+# apihelper.proxy = {"https": PROXY}
 
-TOKEN = "766561573:AAFyg6TptPrXjaDHuBLA-74t9Rc2N6awk7M"
+# TOKEN = "766561573:AAFyg6TptPrXjaDHuBLA-74t9Rc2N6awk7M"
 # TOKEN = os.environ["TOKEN"]
-# TOKEN = os.environ.get("TOKEN")  # using get will return `None` if a key is not present rather than raise a `KeyError`
+TOKEN = os.environ.get("TOKEN")  # using get will return `None` if a key is not present rather than raise a `KeyError`
 bot = telebot.TeleBot(TOKEN)
 
 
